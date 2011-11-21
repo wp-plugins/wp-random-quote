c8de4fc8c71204661b3f7503530f8d20da5f31ca
sabirmostofa <sabirmostofa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-21 16:01:26 +0000

    first
    git-svn-id: https://plugins.svn.wordpress.org/wp-random-quote/trunk@465714 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== WP Random Quote ===
+
+Contributors: sabirmostofa
+Tags: quotes, random, widget, sidebar,  random quotes,quotations, shortcodes
+Requires at least: 2.8
+Tested up to: 3.2.1
+Stable tag: trunk
+
+
+Display a Random Quote In your sidebar using widget or in any page using Shortcode
+
+== Description ==
+
+Add a random quote in a widget or add the random quote in a sidebar widget
+The main features:
+
+* As many **widgets** as you need, Each quotation font sizes are configurable
+* Optional **automatic rotation** of the quotes 
+
+* **Shortcodes** that can be used to add one quote or series of quotes to your posts and pages. The shortcodes come with a set of individual options as well and, if needed, they can be extended to apply everywhere on the blog, allowing random words for the tagline, the category names, the post titles etc.
+
+
+
+
+== Installation ==
+
+1. Upload the content of wp-random-quote.zip to a dedicated folder in your `/wp-content/plugins/` directory.
+2. Activate the plugin on the 'Plugins' page in WordPress.
+
+
+
+
+== Changelog ==
+
---
+++ widget.php
@@ -0,0 +1,71 @@
+<?php
+
+class Random_Quote_Widget extends WP_Widget {
+
+    function __construct() {
+        $widget_ops = array('classname' => 'randomquote', 'description' => 'Display a random quote');
+        $control_ops = array('id_base' => 'random-quotes');
+        $this->WP_Widget('random-quotes', 'Random Quotes', $widget_ops, $control_ops);
+    }
+
+    function form($instance) {
+        // outputs the options form on admin
+        $defs = array(
+            'title' => '',
+            'interval' => 'hourly',
+            'font-size'=>'12px'
+        );
+        $instance = wp_parse_args((array) $instance, $defs);
+        ?>
+        <!--Title -->
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php echo 'Title'; ?>:</label>
+            <input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($instance['title']); ?>" class="widefat" /></p>
+
+        <!-- Check Interval -->
+        <p><label for="<?php echo $this->get_field_id('interval'); ?>"><?php echo 'Interval between fetching the new quotes'; ?>:</label>
+            <select  id="<?php echo $this->get_field_id('interval'); ?>" name="<?php echo $this->get_field_name('interval'); ?>" >
+                <option <?php selected($instance['interval'], 'hourly'); ?> value="hourly">Hourly</option>
+                <option <?php selected($instance['interval'], 'twicedaily'); ?> value="twicedaily" >Twice Daily</option>
+                <option <?php selected($instance['interval'], 'daily'); ?> value="daily" >Daily</option>
+
+            </select>
+         <!-- Font size -->
+                 <p><label for="<?php echo $this->get_field_id('font-size'); ?>"><?php echo 'Font Size(Default: 12px)'; ?>:</label>
+            <input type="text" id="<?php echo $this->get_field_id('font-size'); ?>" name="<?php echo $this->get_field_name('font-size'); ?>" value="<?php echo esc_attr($instance['font-size']); ?>" class="widefat" /></p>
+
+            <?php
+        }
+
+        function update($new_instance, $old_instance) {
+            // processes widget options to be saved  
+            if ($new_instance['interval'] != $old_instance['interval']) {
+                global $random_quote;
+                $random_quote->add_change_cron($new_instance['interval']);
+            }
+
+            return $new_instance;
+        }
+
+        function widget($args, $instance) {
+            // outputs the content of the widget
+            extract($args);
+            global $random_quote;
+            
+            
+            echo $before_widget;
+
+            if (!empty($instance['title']))
+                echo $before_title . apply_filters('widget_title', $instance['title']) . $after_title;
+
+            $quotes = get_option('wprq_random_quotes');
+            if (!$quotes)
+                $quotes = $random_quote->cron_func();
+            $random_quote = $quotes[rand(0, count($quotes))];
+            ?>
+        <div id="wp_random_quote" style="font-size:<?php echo $instance['font-size'] ?>"><?php echo $random_quote ?></div>
+        <?php
+        echo $after_widget;
+    }
+
+}
+
